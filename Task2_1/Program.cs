71bda91fe5cd0f975188ced439f7cd45f0301246
Larisa Samoilenko <qwry28@gmail.com>
2021-08-28 14:14:22 +0300

    Add project files.

---
+++ Task2_1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Tasks2Library;
+
+namespace Task2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(TasksHelper.Task1(a, b));
+        }
+    }
+}
---
+++ Tasks2Library/TasksHelper.cs
@@ -0,0 +1,24 @@
+﻿namespace Tasks2Library
+{
+    public class TasksHelper
+    {
+        public static int Task1(int a, int b)
+        {
+            int result;
+            if (a > b)
+            {
+                result = a + b;
+            }
+            else if (a < b)
+            {
+                result = a - b;
+            }
+            else
+            {
+                result = a * b;
+            }
+
+            return result;
+        }
+    }
+}
