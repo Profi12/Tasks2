2103c930e977605f1ad5c9b07ae7a7b82a1238a3
Larisa Samoilenko <qwry28@gmail.com>
2021-09-17 10:07:23 +0300

    Added Test 3 and 4

--- Tasks2_Tests/Tests.cs
+++ Tasks2_Tests/Tests.cs
@@ -13,6 +13,7 @@ namespace Tasks2_Tests
             Assert.AreEqual(expected, TasksHelper.Task1(a, b));
         }
 
+
         [TestCase(8, 5, 1)]
         [TestCase(-6, -6, 3)]
         [TestCase(-5, 5, 2)]
@@ -23,6 +24,34 @@ namespace Tasks2_Tests
         public void Task2_WhenAnyXAndY_ShouldGetCoordinateQuater(int x, int y, int expected)
         {
             Assert.AreEqual(expected, TasksHelper.Task2(x, y));
+        }
+
+
+        [TestCase(8, 5, 2, 2, 5, 8)]
+        [TestCase(5, 6, 1, 1, 5, 6)]
+        [TestCase(5, 2, 3, 2, 3, 5)]
+        [TestCase(1, 2, 3, 1, 2, 3)]
+        public void Task3_WhenABCPassed_ShouldSortInAscendingOrder(
+             double a,  double b,  double c, double expectedA, double expectedB, double expectedC)
+        {
+            TasksHelper.Task3(ref a, ref b, ref c);
+
+            Assert.AreEqual(expectedA, a);
+            Assert.AreEqual(expectedB, b);
+            Assert.AreEqual(expectedC, c);
+        }
+
+
+        [TestCase(1, -2, -3, 3, -1)]
+        [TestCase(-1, -2, 15, -5, 3)]
+        [TestCase(1, 12, 36, -6, -6)]
+        public void Task4_WhenDMoreZiro_ShouldQuadricRootsOfEquation(
+            double a, double b, double c, double expectedX1, double expectedX2)
+        {
+            QuadricRoots roots = TasksHelper.Task4(a, b, c);
+
+            Assert.True(roots.X1 == expectedX1 || roots.X1 == expectedX2);
+            Assert.True(roots.X2 == expectedX1 || roots.X2 == expectedX2);
         }
     }
 }
